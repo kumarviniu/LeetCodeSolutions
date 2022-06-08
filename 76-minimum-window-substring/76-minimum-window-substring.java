f0d56ca746d8e51384d56b757f3e7c5db809a08c
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-06-08 01:18:54 -0400

    Time: 19 ms (53.62%), Space: 47.5 MB (27.79%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -5,9 +5,6 @@ class Solution {
         
         Map<Character, Integer> tmap = new HashMap<>();
         Map<Character, Integer> smap = new HashMap<>();
-       
-        int need = t.length();
-        int have = 0;
         
         int l = 0;
         int r = 0;
@@ -19,12 +16,15 @@ class Solution {
         for (int i = 0; i < t.length(); i++)
             tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i), 0) + 1);
         
+        int need = tmap.size();
+        int have = 0;
+        
         while (r < s.length()) {
             char c = s.charAt(r);
             if (tmap.containsKey(c)) {
                 int val = smap.getOrDefault(c, 0);
                 smap.put(c, val + 1);
-                if (val + 1 <= tmap.get(c))
+                if (val + 1 == tmap.get(c))
                     have++;
                 while (have == need) {
                     if (r - l + 1 < min) {
