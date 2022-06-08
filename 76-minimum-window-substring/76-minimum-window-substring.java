4e6acdefb8e317f93c624a323e289d05c01244d9
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-06-07 23:37:45 -0400

    Time: 286 ms (7.61%), Space: 54.5 MB (11.01%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length())
+            return "";
+        
+        Map<Character, Integer> tmap = new HashMap<>();
+        for (int i = 0; i < t.length(); i++)
+            tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i), 0) + 1);
+        
+        int l = 0;
+        int r = t.length() - 1;
+        
+        Map<Character, Integer> smap = new HashMap<>();
+        for(int i = 0; i < t.length(); i++)
+            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i), 0) + 1);
+        
+        int min = Integer.MAX_VALUE;
+        int minL = 0;
+        int minR = 0;
+        
+        while (r < s.length()) {
+            if (isMatch(tmap, smap)) {
+                if ((r - l + 1) < min) {
+                    min = r - l + 1;
+                    minL = l;
+                    minR = r;
+                }
+                smap.put(s.charAt(l), smap.get(s.charAt(l)) - 1);
+                l++;
+            } else {
+                r++;
+                if (r < s.length())
+                    smap.put(s.charAt(r), smap.getOrDefault(s.charAt(r), 0) + 1);
+            }
+        }
+        
+        return min != Integer.MAX_VALUE ? s.substring(minL, minR + 1) : "";
+    }
+    
+    public boolean isMatch(Map<Character, Integer> tmap, Map<Character, Integer> smap) {
+        for (Character key : tmap.keySet()) {
+            Integer val = tmap.get(key);
+            if (!smap.containsKey(key) || smap.get(key) < val) return false;
+        }
+        return true;
+    }
+}
