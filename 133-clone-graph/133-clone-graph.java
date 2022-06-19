a94efda93617aa6337a2bd9ac764803ad1047ea3
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-06-19 13:48:53 -0400

    Time: 52 ms (13.47%), Space: 43.1 MB (64.40%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -20,30 +20,18 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
+        return cloneGraph(node, new HashMap<>());
+    }
+    
+    public Node cloneGraph(Node node, Map<Node, Node> map) {
         if (node == null)
             return null;
-        
-        Map<Node, Node> map = new HashMap<>();
-        Queue<Pair<Node, Node>> q = new LinkedList<>();
-        Node root = new Node(node.val, new ArrayList<>());
-        q.add(new Pair<>(node, root));
-        map.put(node, root);
-        
-        while (!q.isEmpty()) {
-            Pair<Node, Node> pair = q.poll();
-            Node old = pair.getKey();
-            Node newNode = pair.getValue();
-            for (Node neighbor : old.neighbors) {
-                Node newNeighbor;
-                if (!map.containsKey(neighbor)) {
-                    newNeighbor = new Node(neighbor.val, new ArrayList<>());
-                    q.add(new Pair<>(neighbor, newNeighbor));
-                    map.put(neighbor, newNeighbor);
-                } else
-                    newNeighbor = map.get(neighbor);
-                newNode.neighbors.add(newNeighbor);
-            }
-        }
-        return root;
+        if (map.containsKey(node))
+            return map.get(node);
+        Node copy = new Node(node.val, new ArrayList<>());
+        map.put(node, copy);
+        for (Node neighbor : node.neighbors)
+            copy.neighbors.add(cloneGraph(neighbor, map));
+        return copy;
     }
 }
