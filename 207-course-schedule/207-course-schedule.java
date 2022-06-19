592e45fab72860cdc167dfc66e5222692475254f
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-06-19 18:49:41 -0400

    Time: 5 ms (84.20%), Space: 48.2 MB (50.10%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+             
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        
+        for(int i = 0; i < numCourses; i++)
+            graph.put(i, new ArrayList<>());
+        
+        for (int[] pre : prerequisites)
+            graph.get(pre[1]).add(pre[0]);
+        
+        int[] visited = new int[numCourses];
+        
+        for(int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0 && !dfs(i, graph, visited))
+                return false;
+        }
+        return true;
+    }
+    
+    public boolean dfs(int vertex, Map<Integer, List<Integer>> graph, int[] visited) {
+        visited[vertex] = 1;
+        for (Integer neighbor : graph.get(vertex)) {
+            if (visited[neighbor] == 1 || (visited[neighbor] == 0 && !dfs(neighbor, graph, visited)))
+                return false;
+        }
+        visited[vertex] = 2;
+        return true;
+    }
+}
