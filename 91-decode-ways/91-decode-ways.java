e1adf25a42089ad2e1b5bfc232fc714f5ae251d1
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-06-24 21:58:12 -0400

    Time: 1 ms (98.20%), Space: 42.5 MB (44.19%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,27 +1,20 @@
 class Solution {
     public int numDecodings(String s) {
-        return dfs(s, 0, new HashMap<>());
-    }
-    
-    public int dfs(String s, int index, Map<Integer, Integer> map) {
-        if (index == s.length())
-            return 1;
+        int a = 1;
+        int b = 0;
         
-        if (s.charAt(index) == '0')
-            return 0;
-        
-        if (map.containsKey(index))
-            return map.get(index);
-        
-        int result = dfs(s, index + 1, map);
-        
-        if (index + 1 < s.length()
-            && (s.charAt(index) == '1' 
-                || (s.charAt(index) == '2' && s.charAt(index + 1) >= '0' && s.charAt(index + 1) <= '6')))
-            result += dfs(s, index + 2, map);
-        
-        map.put(index, result);
-        
-        return result;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int result = 0;
+            if (s.charAt(i) != '0') {
+                result = a;
+            }
+            if (i + 1 < s.length() && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '6'))) {
+                result += b;
+            }
+            
+            b = a;
+            a = result;
+        }
+        return a;
     }
 }
