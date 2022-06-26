b420cae6f407262a067ac40e5e0885235b6dd125
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-06-26 19:40:40 -0400

    Time: 134 ms (14.41%), Space: 61.4 MB (85.63%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<Pair<Integer, Integer>> maxPq = new PriorityQueue<>((a, b) -> Integer.compare(b.getValue(), a.getValue()));
-        Set<Integer> removed = new HashSet<>();
+        PriorityQueue<Pair<Integer, Integer>> maxPq = new PriorityQueue<>((a, b) -> Integer.compare(b.getValue(), a.getValue()));      
         int j = 0;
         while (j < k) {
             maxPq.add(new Pair<>(j, nums[j]));
@@ -13,12 +12,11 @@ class Solution {
         int i = 0;
         while (j < nums.length) {
             Pair<Integer, Integer> pair = maxPq.peek();
-            while (removed.contains(pair.getKey())) {
+            while (pair.getKey() < i) {
                 maxPq.poll();
                 pair = maxPq.peek();
             }
             result[idx] = pair.getValue();
-            removed.add(i);
             if (j + 1 < nums.length)
                 maxPq.add(new Pair<>(j + 1, nums[j + 1]));
             idx++;
