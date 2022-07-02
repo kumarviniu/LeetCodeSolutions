82e3b25c7f9bab74028530f27e4fe01b42b20ec5
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-07-01 21:13:50 -0400

    Time: 11 ms (5.09%), Space: 48.4 MB (8.80%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -2,47 +2,37 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
         List<List<Integer>> result = new ArrayList<>();
-        if (intervals.length == 0 || newInterval[1] < intervals[0][0]) {
-            List<Integer> single = new ArrayList<>();
-            single.add(newInterval[0]);
-            single.add(newInterval[1]);
-            result.add(single);
-        }
-        
-        if (intervals.length > 0 && newInterval[0] > intervals[n - 1][1]) {
-            intervals = Arrays.copyOf(intervals, n + 1);
-            intervals[n] = new int[2];
-            intervals[n][0] = newInterval[0];
-            intervals[n][1] = newInterval[1];
-            return intervals;
-        }
-                
-        //merge
+        boolean isMerged = false;
         for (int i = 0; i < n; i++) {
-            int[] interval = intervals[i];
-            List<Integer> out = new ArrayList<>();
-            if ((newInterval[0] <= interval[1] && newInterval[0] >= interval[0]) || (interval[0] <= newInterval[1] && interval[0] >= newInterval[0])) {
-                out.add(Math.min(interval[0], newInterval[0]));
-                while (newInterval[1] >= interval[1] && i + 1 < n && newInterval[1] >= intervals[i + 1][0]) {
-                    i++;
-                interval = intervals[i];
-                }
-                out.add(Math.max(newInterval[1], interval[1]));
-            } else if (i + 1 < n && newInterval[0] > interval[1] && newInterval[1] < intervals[i + 1][0]) {
-                List<Integer> out2 = new ArrayList<>();
-                out2.add(interval[0]);
-                out2.add(interval[1]);
-                result.add(out2);
-                
+            if (newInterval[1] < intervals[i][0]) {
+                isMerged = true;
+                List<Integer> out = new ArrayList<>();
                 out.add(newInterval[0]);
                 out.add(newInterval[1]);
+                result.add(out);
+                while(i < n) {
+                    out = new ArrayList<>();
+                    out.add(intervals[i][0]);
+                    out.add(intervals[i][1]);
+                    result.add(out);
+                    i++;
+                }
+            } else if (newInterval[0] > intervals[i][1]) {
+                List<Integer> out = new ArrayList<>();
+                out.add(intervals[i][0]);
+                out.add(intervals[i][1]);
+                result.add(out);
             } else {
-                out.add(interval[0]);
-                out.add(interval[1]);
+                newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+                newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
             }
+        }
+        if (!isMerged) {
+            List<Integer> out = new ArrayList<>();
+            out.add(newInterval[0]);
+            out.add(newInterval[1]);
             result.add(out);
-        } 
-       
+        }
         int[][] arr = result.stream()
                 .map(l -> l.stream().mapToInt(Integer::intValue).toArray())
                 .toArray(int[][]::new);
