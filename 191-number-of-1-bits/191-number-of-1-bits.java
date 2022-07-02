1f5be065e2d287f07a1295c72c4dbfb7ade91a7d
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-07-02 18:19:11 -0400

    Time: 1 ms (85.29%), Space: 41.1 MB (54.68%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -3,8 +3,7 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         for (int i = 0; i < 32; i++) {
-            if ((n & 1) == 1)
-                count++;
+            count += n & 1;
             n >>= 1;
         }
         return count;
