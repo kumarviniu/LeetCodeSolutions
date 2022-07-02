d3b42d6b3ef2375cddd87f0bf6e51ca8258ece8d
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-07-02 18:01:25 -0400

    Time: 79 ms (68.33%), Space: 100.2 MB (58.48%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -3,9 +3,8 @@ class Solution {
         Arrays.sort(intervals, (a, b)-> Integer.compare(a[0], b[0]));
         int min = 0;
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] >= intervals[i - 1][0] && intervals[i][0] < intervals[i - 1][1]) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
                 min++;
-                intervals[i][0] = Math.min(intervals[i][0], intervals[i - 1][0]);
                 intervals[i][1] = Math.min(intervals[i][1], intervals[i - 1][1]);
             }
         }
