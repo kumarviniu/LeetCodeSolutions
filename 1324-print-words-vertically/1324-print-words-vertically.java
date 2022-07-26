6e0bc978e2ec579af12132ca86c7f9b8b1f66d3a
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-07-25 23:54:28 -0400

    Time: 1 ms (95.39%), Space: 42 MB (79.26%) - LeetHub

---
+++ 1324-print-words-vertically/1324-print-words-vertically.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> printVertically(String s) {
+        String[] split = s.split(" ");
+        int idx = 0;
+        int i = 0;
+        boolean isValid = false;
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("");
+        int lastChar = 0;
+        while (true) {
+            if (idx < split[i].length()) {
+                sb.append(split[i].charAt(idx));
+                isValid = true;
+                lastChar = sb.length();
+            } else
+                sb.append(" ");
+            if (i == split.length - 1 && !isValid)
+                break;
+            if (i == split.length - 1) {
+                result.add(sb.substring(0, lastChar));
+                sb = new StringBuilder("");
+                isValid = false;
+                idx++;
+                lastChar = 0;
+            }
+            i++;
+            i %= split.length;
+        }
+        return result;
+    }
+}
