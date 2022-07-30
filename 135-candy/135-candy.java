961c53be1beafc15d6e89d07dd3c571eca135c3c
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-07-30 19:25:33 -0400

    Time: 2 ms (99.96%), Space: 52.2 MB (23.67%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,20 +1,28 @@
 class Solution {
     public int candy(int[] ratings) {
-        int[] rewards = new int[ratings.length];
-    Arrays.fill(rewards, 1);
-    for (int i = 1; i < ratings.length; i++) {
-      if (ratings[i] > ratings[i - 1])
-        rewards[i] += rewards[i - 1];
-    }
-
-    for (int i = ratings.length - 2; i >= 0; i--) {
-      if (ratings[i] > ratings[i + 1])
-        rewards[i] = Math.max(rewards[i], rewards[i + 1] + 1);
-    }
-
-    int total = 0;
-    for (int reward: rewards)
-      total += reward;
-    return total;
+        int lastMaxReward = 1;
+        int lastMaxIndex = 0;
+        int lastAllocatedReward = 1;
+        int totalRewards = 1;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                lastAllocatedReward++;
+                lastMaxIndex = i;
+                lastMaxReward = lastAllocatedReward;
+                totalRewards += lastAllocatedReward;
+            } else if (ratings[i] == ratings[i - 1]) {
+                totalRewards++;
+                lastAllocatedReward = 1;
+                lastMaxReward = 1;
+                lastMaxIndex = i;
+            } else if (lastAllocatedReward > 1) {
+                lastAllocatedReward = 1;
+                totalRewards += lastAllocatedReward;
+            } else {
+                totalRewards += i - lastMaxIndex;
+                if (i - lastMaxIndex >= lastMaxReward) totalRewards++;
+            }
+        }
+        return totalRewards;
     }
 }
