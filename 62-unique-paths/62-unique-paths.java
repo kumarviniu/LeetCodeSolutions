af8137a2a5811d959ada2504e5d9dc0f8c7fc792
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-04 01:37:20 -0400

    Time: 3 ms (5.98%), Space: 41.7 MB (7.80%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,11 +1,14 @@
 import java.math.BigInteger;
 class Solution {
     public int uniquePaths(int m, int n) {
-        return factorial((m - 1) + (n - 1)).divide(factorial(m - 1).multiply(factorial(n - 1))).intValue();
+        m = m - 1;
+        n = n - 1;
+        return factorial(m + n, m + 1).divide(factorial(n, 1)).intValue();
     }
-    BigInteger factorial(int n) {
+    BigInteger factorial(int n, int r) {
+        n = r > n ? 1 : n;
         BigInteger b = new BigInteger(String.valueOf(Math.max(1, n)));
-        while(n-- > 1)
+        while(n-- > r)
             b = b.multiply(new BigInteger(String.valueOf(n)));
         return b;
     }
