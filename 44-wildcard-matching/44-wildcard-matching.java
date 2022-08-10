9a92906a03f7d00d1c80326958873d91ead8dc0d
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-09 23:24:32 -0400

    Time: 58 ms (31.12%), Space: 42.4 MB (98.45%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
+        boolean[][] dp = new boolean[2][s.length() + 1];
         dp[0][0] = true;
         for (int i = 0; i <= p.length(); i++) {
             for (int j = 0; j <= s.length(); j++) {
                 if (i == 0)
-                    dp[i][j] = j == 0;
+                    dp[i % 2][j] = j == 0;
                 else if (j == 0)
-                    dp[i][j] = p.charAt(i - 1) == '*' ? dp[i - 1][0] : false;
+                    dp[i % 2][j] = p.charAt(i - 1) == '*' ? dp[(i - 1) % 2][0] : false;
                 else if (p.charAt(i - 1) ==  '*')
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    dp[i % 2][j] = dp[(i - 1) % 2][j] || dp[i % 2][j - 1];
                 else if (p.charAt(i - 1) == s.charAt(j - 1) || p.charAt(i - 1) == '?')
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1];
                 else
-                    dp[i][j] = false;
+                    dp[i % 2][j] = false;
             }
         }
-        return dp[p.length()][s.length()];
+        return dp[p.length() % 2][s.length()];
     }
 }
