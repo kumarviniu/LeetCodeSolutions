5af3e645b002052a851dbe1763613bbe5d5b68c4
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-09 23:17:13 -0400

    Time: 43 ms (55.24%), Space: 48.8 MB (45.14%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -2,15 +2,13 @@ class Solution {
     public boolean isMatch(String s, String p) {
         boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
         dp[0][0] = true;
-        for (int i = 1; i <= s.length(); i++)
-            dp[0][i] = false;
-        
-        for (int i = 1; i <= p.length(); i++)
-            dp[i][0] = p.charAt(i - 1) == '*' ? dp[i - 1][0] : false;
-        
-        for (int i = 1; i <= p.length(); i++) {
-            for (int j = 1; j <= s.length(); j++) {
-                if (p.charAt(i - 1) ==  '*')
+        for (int i = 0; i <= p.length(); i++) {
+            for (int j = 0; j <= s.length(); j++) {
+                if (i == 0)
+                    dp[i][j] = j == 0;
+                else if (j == 0)
+                    dp[i][j] = p.charAt(i - 1) == '*' ? dp[i - 1][0] : false;
+                else if (p.charAt(i - 1) ==  '*')
                     dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
                 else if (p.charAt(i - 1) == s.charAt(j - 1) || p.charAt(i - 1) == '?')
                     dp[i][j] = dp[i - 1][j - 1];
