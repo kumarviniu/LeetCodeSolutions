aa63d645eb21e3c853fe5d7686033d83fdac582e
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-09 20:13:15 -0400

    Time: 1 ms (98.92%), Space: 41.5 MB (32.87%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -3,20 +3,21 @@ class Solution {
         if (n == 1)
             return true;
         
-        int newN = getNewN(String.valueOf(n));
+        int newN = getNewN(n);
         
         while (newN != 1 && n != newN) {
-            n = getNewN(String.valueOf(n));
-            newN = getNewN(String.valueOf(getNewN(String.valueOf(newN))));
+            n = getNewN(n);
+            newN = getNewN(getNewN(newN));
         }
         return newN == 1;
     }
     
-    public int getNewN(String nString) {
+    public int getNewN(int n) {
         int newN = 0;
-        for (int i = 0; i <nString.length(); i++) {
-            int val = Character.getNumericValue(nString.charAt(i));
-            newN += val * val;
+        while (n != 0) {
+            int mod = n % 10;
+            newN += mod * mod;
+            n /= 10;
         }
         return newN;
     }
