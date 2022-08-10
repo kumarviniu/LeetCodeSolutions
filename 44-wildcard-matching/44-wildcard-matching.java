f5cba50757eb808d1e62cd2a21aa381ace7fc8f9
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-09 23:14:21 -0400

    Time: 46 ms (49.48%), Space: 48.8 MB (45.14%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,47 +1,23 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return isMatch(s, p, 0, 0, new Boolean[p.length()][s.length()]);
-    }
-    
-    public boolean isMatch(String s, String p, int pIdx, int sIdx, Boolean[][] dp) {
-        //pattern is empty, but string isn't
-        if (pIdx >= p.length() && sIdx >= s.length())
-            return true;
+        boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s.length(); i++)
+            dp[0][i] = false;
         
-        if (pIdx >= p.length())
-            return false;
+        for (int i = 1; i <= p.length(); i++)
+            dp[i][0] = p.charAt(i - 1) == '*' ? dp[i - 1][0] : false;
         
-        //string is empty, but pattern isnt
-        if (pIdx < p.length() && sIdx >= s.length()) {
-            for (int i = pIdx; i < p.length(); i++)
-                if (p.charAt(i) != '*')
-                    return false;
-            return true;
+        for (int i = 1; i <= p.length(); i++) {
+            for (int j = 1; j <= s.length(); j++) {
+                if (p.charAt(i - 1) ==  '*')
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                else if (p.charAt(i - 1) == s.charAt(j - 1) || p.charAt(i - 1) == '?')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = false;
+            }
         }
-        
-        //pattern's last element is *, so don't care about string
-        if (pIdx == p.length() - 1) {
-            if (p.charAt(pIdx) == '*')
-                return true;
-        }
-        
-        if (dp[pIdx][sIdx] != null)
-            return dp[pIdx][sIdx];
-        
-        //if any other character and doesn't match
-        if (p.charAt(pIdx) != '*' && p.charAt(pIdx) != '?') {
-             if(p.charAt(pIdx) != s.charAt(sIdx))
-                return dp[pIdx][sIdx] = false;
-            return dp[pIdx][sIdx] = isMatch(s, p, pIdx + 1, sIdx + 1, dp);
-        }
-        
-        //if ?
-        if (p.charAt(pIdx) == '?') {
-            if (sIdx >= s.length())
-                return dp[pIdx][sIdx] = false;
-            return dp[pIdx][sIdx] = isMatch(s, p, pIdx + 1, sIdx + 1, dp);
-        }
-        
-        return dp[pIdx][sIdx] = isMatch(s, p, pIdx + 1, sIdx, dp) || isMatch(s, p, pIdx, sIdx + 1, dp);
+        return dp[p.length()][s.length()];
     }
 }
