52c68bfcb4aff21b2f6bb24eeef1c67451f0ce3b
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-13 13:54:49 -0400

    Time: 11 ms (86.98%), Space: 42.5 MB (93.95%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,68 +1,50 @@
 class Solution {
-
-    private static final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
     public int shortestPath(int[][] grid, int k) {
-
-        int[] target = new int[]{grid.length - 1, grid[0].length - 1};
-
-        Set<Entry> visited = new HashSet<>();
-
-        Queue<Entry> queue = new LinkedList<>();
-        queue.offer(new Entry(0, 0, 0, 0));
-
-        while (!queue.isEmpty()) {
-
-            Entry currEntry = queue.poll();
-            if (currEntry.row == target[0] && currEntry.col == target[1]) return currEntry.level;
-
-            for (int[] dir : DIRS) {
-
-                int nextRow = currEntry.row + dir[0];
-                int nextCol = currEntry.col + dir[1];
-
-                boolean inBound = nextRow >= 0 && nextRow < grid.length && nextCol >= 0 && nextCol < grid[0].length;
-                if (inBound) {
-
-                    int newObstacles = currEntry.obstacles + grid[nextRow][nextCol];
-                    if (newObstacles <= k) {
-
-                        Entry newEntry = new Entry(nextRow, nextCol, newObstacles, currEntry.level + 1);
-                        if (!visited.contains(newEntry)) {
-                            visited.add(newEntry);
-                            queue.offer(newEntry);
-                        }
+        Integer[][] remainingObstacles = new Integer[grid.length][grid[0].length];
+        int[][] directions = {
+            {0, -1},
+            {0, 1},
+            {-1, 0},
+            {1, 0}
+        };
+        Queue<Cell> q = new LinkedList<>();
+        q.add(new Cell(0, 0, 0,0));
+        remainingObstacles[0][0] = 0;
+        while (!q.isEmpty()) {
+            Cell cell = q.poll();
+            if (cell.row == grid.length - 1 && cell.col == grid[0].length - 1)
+                return cell.path;
+            for (int i = 0; i < directions.length; i++) {
+                int row = cell.row + directions[i][0];
+                int col = cell.col + directions[i][1];
+                if (isValid(grid, row, col)) {
+                    int newObstacles = cell.pathObstacles + grid[row][col];
+                    int newRemainingObstacles = k - (cell.pathObstacles + grid[row][col]);
+                    if (newObstacles <= k && (remainingObstacles[row][col] == null || newRemainingObstacles > remainingObstacles[row][col])) {
+                        remainingObstacles[row][col] = newRemainingObstacles;
+                        q.add(new Cell(row, col, cell.pathObstacles + grid[row][col], cell.path + 1));
                     }
                 }
             }
         }
         return -1;
     }
-
-    private static class Entry {
+    
+    boolean isValid(int[][] grid, int i, int j) {
+        return i >= 0 && j >= 0 && i < grid.length && j < grid[0].length;
+    }
+    
+    class Cell {
         int row;
         int col;
-        int obstacles;
-        int level;
-
-        Entry(int row, int col, int obstacles, int level) {
-
-            this.row = row;
-            this.col = col;
-            this.obstacles = obstacles;
-            this.level = level;
-        }
-
-        public boolean equals(Object that) {
-            if (that == null) return false;
-            if (this == that) return true;
-
-            Entry entry = (Entry) that;
-            return Objects.equals(this.row, entry.row) && Objects.equals(this.col, entry.col) && Objects.equals(this.obstacles, entry.obstacles);
-        }
-
-        public int hashCode() {
-            return Objects.hash(row, col, obstacles);
+        int pathObstacles = 0;
+        int path = 0;
+        
+        Cell(int i, int j, int o, int p) {
+            row = i;
+            col = j;
+            pathObstacles = o;
+            path = p;
         }
     }
 }
