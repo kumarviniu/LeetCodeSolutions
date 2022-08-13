5edb9c1206273e9436421b4192c1b1d52afc33b1
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-13 15:15:53 -0400

    Time: 321 ms (36.74%), Space: 124.8 MB (70.14%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
@@ -8,12 +8,11 @@ class Solution {
         };
         Integer[][] minObstacles = new Integer[grid.length][grid[0].length];
         PriorityQueue<Cell> q = new PriorityQueue<>((a, b) -> Integer.compare(a.pathObstacles, b.pathObstacles));
-        int minObstaclesToReach = grid.length + grid[0].length;
         q.add(new Cell(0, 0, 0));
         while (!q.isEmpty()) {
             Cell cell = q.poll();
             if (cell.row == grid.length - 1 && cell.col == grid[0].length - 1)
-                minObstaclesToReach = Math.min(minObstaclesToReach, cell.pathObstacles);
+                return cell.pathObstacles;
             for (int[] direction : directions) {
                 int row = cell.row + direction[0];
                 int col = cell.col + direction[1];
@@ -26,7 +25,7 @@ class Solution {
                 }
             }
         }
-        return minObstaclesToReach;
+        return 0;
     }
     
     boolean isValid(int[][] grid, int i, int j) {
