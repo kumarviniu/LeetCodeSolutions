5affd8eb762b318e32fa06a1c40aaa6711624d9d
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-08-17 16:04:58 -0400

    Time: 7 ms (81.01%), Space: 43.7 MB (37.12%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -6,29 +6,28 @@ class Solution {
         char[] s1FreqArray = new char[26];
         char[] s2WindowFreqArray = new char[26];
         
-        int start = 0;
+        
         int s1Size = s1.length();
         int have = 0;
-        int required = 0;
+        int required = s1Size;
+        int start = 1 - s1Size;
         
         for (int i = 0; i < s1.length(); i++) {
             int charIdx = s1.charAt(i) - 97;
             s1FreqArray[charIdx]++;
-            required++;
         }
      
         for (int i = 0; i < s2.length(); i++) {
-            int windowSize = (i - start) + 1;
             int charIdx = s2.charAt(i) - 97;
             s2WindowFreqArray[charIdx]++;
             if (s2WindowFreqArray[charIdx] <= s1FreqArray[charIdx]) have++;
-            if (windowSize == s1Size) {
+            if (start >= 0) {
                 if (have == required) return true;
                 int startCharIdx = s2.charAt(start) - 97;
                 s2WindowFreqArray[startCharIdx]--;
                 if (s2WindowFreqArray[startCharIdx] < s1FreqArray[startCharIdx]) have--;
-                start++;
             }
+            start++;
         }
         return false;
     }
