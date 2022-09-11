dc88ab763d72ca829277576d73966a8fb30d3392
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-10 23:06:03 -0400

    Time: 33 ms (89.95%), Space: 136.9 MB (89.20%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,33 +1,29 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = createGraph(edges);
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-        queue.add(source);
-        visited.add(source);
-        while (!queue.isEmpty()) {
-            Integer vertex = queue.poll();
-            if (vertex == destination) return true;
-            for (Integer neighbor : graph.get(vertex)) {
-                if (!visited.contains(neighbor)) {
-                    visited.add(neighbor);
-                    queue.add(neighbor);
-                }
-            }
-        }
-        return false;
-    }
-    
-    Map<Integer, List<Integer>> createGraph(int[][] edges) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] edge: edges) {
-            List<Integer> out = graph.getOrDefault(edge[0], new ArrayList<>());
-            List<Integer> in = graph.getOrDefault(edge[1], new ArrayList<>());
-            out.add(edge[1]);
-            in.add(edge[0]);
-            graph.put(edge[0], out);
-            graph.put(edge[1], in);
-        }
-        return graph;
+    if (source == destination) return true;
+    Queue<Integer> q = new LinkedList<>();
+	boolean[] visited = new boolean[n];	
+	q.add(source);
+	visited[source] = true;
+	while(!q.isEmpty()) {
+		Integer s = q.poll();
+		for(int k = 0; k < edges.length; k++) {
+			if (edges[k][0] == s) {
+				if (edges[k][1] == destination) return true;
+				if (!visited[edges[k][1]]) {
+					visited[edges[k][1]] = true;
+					q.add(edges[k][1]);
+				}
+			}
+            else if (edges[k][1] == s) {
+				if (edges[k][0] == destination) return true;
+				if (!visited[edges[k][0]]) {
+					visited[edges[k][0]] = true;
+					q.add(edges[k][0]);
+				}
+			}
+		}
+	}
+	return false;
     }
 }
