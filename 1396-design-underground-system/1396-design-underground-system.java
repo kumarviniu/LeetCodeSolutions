0b95b65b07e5b0283e124b8d02a9e06d0f0430e6
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-21 20:54:43 -0400

    Time: 231 ms (33.42%), Space: 104.4 MB (29.08%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,5 +1,5 @@
 class UndergroundSystem {
-    Map<String, Map<String, Integer[]>> stationAverage;
+    Map<String, Integer[]> stationAverage;
     Map<Integer, StationCheckIn> checkInTime;
     public UndergroundSystem() {
         stationAverage = new HashMap<>();
@@ -13,21 +13,21 @@ class UndergroundSystem {
     public void checkOut(int id, String stationName, int t) {
         StationCheckIn checkedInStation = checkInTime.get(id);
         int timeToTravel = t - checkedInStation.checkInTime;
-        Map<String, Integer[]> timeTravel = stationAverage.getOrDefault(checkedInStation.station, new HashMap<>());
-        Integer[] times = timeTravel.getOrDefault(stationName, new Integer[2]);
+        String stationsId = checkedInStation.station + " " + stationName;
+        Integer[] times = stationAverage.getOrDefault(stationsId, new Integer[2]);
         if (times[0] == null) {
             times[0] = 0;
             times[1] = 0;
         }
         times[0] += timeToTravel;
         times[1]++;
-        timeTravel.putIfAbsent(stationName, times);
-        stationAverage.putIfAbsent(checkedInStation.station, timeTravel);
+        
+        stationAverage.putIfAbsent(stationsId, times);
         checkInTime.remove(id);
     }
     
     public double getAverageTime(String startStation, String endStation) {
-        Integer[] sumAndCount = stationAverage.get(startStation).get(endStation);
+        Integer[] sumAndCount = stationAverage.get(startStation + " " + endStation);
         return (double) sumAndCount[0] / sumAndCount[1];
     }
     
