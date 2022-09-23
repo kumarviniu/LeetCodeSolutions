dc645746a99d1672b435ae5ca6a6826e50e2f950
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-22 21:34:56 -0400

    Time: 4 ms (34.63%), Space: 42.1 MB (61.83%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,23 +1,21 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        int n = costs.length / 2;
-        return dfs(costs, n, 0, 0, 0, new int[n + 1][n + 1]);
-    }
-    
-    int dfs(int[][] costs, int n, int i, int aCount, int bCount, int[][] dp) {
-        if (i >= costs.length) return 0;
-        if (dp[aCount][bCount] != 0) return dp[aCount][bCount];
-        int a = 0;
-        int b = 0;
-        if (aCount < n)
-            a = costs[i][0] + dfs(costs, n, i + 1, aCount + 1, bCount, dp);
-        else
-            return dp[aCount][bCount] = costs[i][1] + dfs(costs, n, i + 1, aCount, bCount + 1, dp);
+        int n = costs.length;
+        int[][] differences = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            differences[i][0] = costs[i][1] - costs[i][0];
+            differences[i][1] = i;
+        }
+        Arrays.sort(differences, (a, b) -> Integer.compare(a[0], b[0]));
         
-        if (bCount < n)
-            b = costs[i][1] + dfs(costs, n, i + 1, aCount, bCount + 1, dp);
-        else
-            return dp[aCount][bCount] = costs[i][0] + dfs(costs, n, i + 1, aCount + 1, bCount, dp);
-        return dp[aCount][bCount] = Math.min(a, b);
+        int half = n / 2;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i < half)
+                sum += costs[differences[i][1]][1];
+            else
+                sum += costs[differences[i][1]][0];
+        }
+        return sum;
     }
 }
