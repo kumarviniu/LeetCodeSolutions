59ea0256e269b47360c9c224d244d646dbd7ce08
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-22 20:18:30 -0400

    Time: 123 ms (49.27%), Space: 81.3 MB (83.43%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,43 @@
+class BrowserHistory {
+    List<String> visitedPages;
+    int currentIdx;
+    int end = 0;
+    
+    public BrowserHistory(String homepage) {
+        visitedPages = new ArrayList<>();
+        visitedPages.add(homepage);
+        currentIdx = 0;
+    }
+    
+    public void visit(String url) {
+        if (currentIdx < end || end < visitedPages.size() - 1) {
+            visitedPages.set(currentIdx + 1, url);
+            currentIdx++;
+            end = currentIdx;
+        } else {
+            visitedPages.add(url);
+            currentIdx++;
+            end++;
+        }
+    }
+    
+    public String back(int steps) {
+        int idx = Math.max(0, currentIdx - steps);
+        currentIdx = idx;
+        return visitedPages.get(currentIdx);
+    }
+    
+    public String forward(int steps) {
+        int idx = Math.min(end, currentIdx + steps);
+        currentIdx = idx;
+        return visitedPages.get(currentIdx);
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
