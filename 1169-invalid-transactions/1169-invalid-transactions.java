9a74d2deaceaccb9d1ca9e167633c68e8748d7a4
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-22 23:24:56 -0400

    Time: 7 ms (99.75%), Space: 43 MB (91.78%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.java
+++ 1169-invalid-transactions/1169-invalid-transactions.java
@@ -1,27 +1,30 @@
 class Solution {
     public List<String> invalidTransactions(String[] transactions) {
         int n = transactions.length;
-        Transaction[] trxns = new Transaction[n];
+        Map<String, List<Transaction>> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            String transaction = transactions[i];
-            trxns[i] = Transaction.parse(transaction);
+            Transaction transaction = Transaction.parse(transactions[i], i);
+            List<Transaction> nameTransactions = map.getOrDefault(transaction.name, new ArrayList<>());
+            nameTransactions.add(transaction);
+            map.putIfAbsent(transaction.name, nameTransactions);
         }
+        
         List<String> invalidTransactions = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (isInvalid(trxns, i))
-                invalidTransactions.add(transactions[i]);
+        for (String name : map.keySet()) {
+            List<Transaction> neighbors = map.get(name);
+            for (Transaction t : neighbors)
+                if (isInvalid(t, neighbors))
+                    invalidTransactions.add(transactions[t.idx]);
         }
         return invalidTransactions;
     }
     
-    boolean isInvalid(Transaction[] trxns, int idx) {
-        Transaction transaction = trxns[idx];
+    boolean isInvalid(Transaction transaction, List<Transaction> neighbors) {
         if (transaction.amount > 1000) return true;
-        for (int i = 0; i < trxns.length; i++) {
-            if (i != idx
-                && trxns[i].name.equals(transaction.name) 
-                && !transaction.city.equals(trxns[i].city) 
-                && Math.abs(trxns[i].time - transaction.time) <= 60)
+        for (Transaction t : neighbors) {
+            if (t.name.equals(transaction.name) 
+                && !transaction.city.equals(t.city) 
+                && Math.abs(t.time - transaction.time) <= 60)
                 return true;
         }
         return false;
@@ -32,21 +35,23 @@ class Solution {
         int time;
         int amount;
         String city;
+        int idx;
         
-        Transaction(String n, int t, int a, String c) {
+        Transaction(String n, int t, int a, String c, int i) {
             name = n;
             time = t;
             amount = a;
             city = c;
+            idx = i;
         }
         
-        static Transaction parse(String transaction) {
+        static Transaction parse(String transaction, int idx) {
             String[] split = transaction.split(",");
             String name = split[0];
             int time = Integer.parseInt(split[1]);
             int amount = Integer.parseInt(split[2]);
             String city = split[3];
-            return new Transaction(name, time, amount, city);
+            return new Transaction(name, time, amount, city, idx);
         }
     }
 }
