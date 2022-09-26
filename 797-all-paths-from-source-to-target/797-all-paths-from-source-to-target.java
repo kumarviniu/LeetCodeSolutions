d378da271a7841a2801d6b4e281d524eddfdc910
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-25 20:51:42 -0400

    Time: 10 ms (20.16%), Space: 55.5 MB (19.05%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,26 +1,32 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        return pathByDfs(graph, 0);
+        return pathByDfs(graph, 0, new HashMap<>());
     }
     
-    public List<List<Integer>> pathByDfs(int[][] graph, int node) {
+    public List<List<Integer>> pathByDfs(int[][] graph, int node, Map<Integer, List<List<Integer>>> dp) {
         if (node == graph.length - 1) {
             List<List<Integer>> paths = new LinkedList<>();
             List<Integer> path = new LinkedList<>();
             path.add(node);
-            paths.add(path);
+            paths.add(path);            
             return paths;
         }
         
+        if (dp.containsKey(node))
+            return dp.get(node);
+        
         List<List<Integer>> paths = new LinkedList<>();
         for (int neighbor : graph[node]) {
-            List<List<Integer>> neighborPaths = pathByDfs(graph, neighbor);
+            List<List<Integer>> neighborPaths = pathByDfs(graph, neighbor, dp);
             for (List<Integer> path : neighborPaths) {
-                path.add(0, node);
-                paths.add(path);
+                List<Integer> newPath = new ArrayList<>(path.size() + 1);
+                newPath.add(node);
+                newPath.addAll(path);
+                paths.add(newPath);
             }
         }
         
+        dp.put(node, paths);
         return paths;
     }
 }
