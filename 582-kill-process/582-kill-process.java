6264a04c9dea4480b18b3f04de34512592277c6c
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-26 02:02:49 -0400

    Time: 21 ms (98.34%), Space: 68.5 MB (81.86%) - LeetHub

--- 582-kill-process/582-kill-process.java
+++ 582-kill-process/582-kill-process.java
@@ -1,6 +1,12 @@
 class Solution {
     public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
-        Map<Integer, List<Integer>> adjList = adjList(pid, ppid);
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        for (int i = 0; i < pid.size(); i++) {
+            if (kill == pid.get(i) && ppid.get(i) == 0) return pid;
+            List<Integer> children = adjList.getOrDefault(ppid.get(i), new ArrayList<>());
+            children.add(pid.get(i));
+            adjList.putIfAbsent(ppid.get(i), children);
+        }
         List<Integer> result = new ArrayList<>();
         dfs(adjList, kill, result);
         return result;
@@ -10,15 +16,5 @@ class Solution {
         result.add(kill);
         if (!adjList.containsKey(kill)) return;
         for (Integer child : adjList.get(kill)) dfs(adjList, child, result);
-    }
-    
-    public Map<Integer, List<Integer>> adjList(List<Integer> pid, List<Integer> ppid) {
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
-        for (int i = 0; i < pid.size(); i++) {
-            List<Integer> children = adjList.getOrDefault(ppid.get(i), new ArrayList<>());
-            children.add(pid.get(i));
-            adjList.putIfAbsent(ppid.get(i), children);
-        }
-        return adjList;
     }
 }
