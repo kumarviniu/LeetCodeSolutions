4f7931e7a568090474a4e23fc7e5a8e3be1ae5a5
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-25 20:29:02 -0400

    Time: 7 ms (48.26%), Space: 40.1 MB (99.78%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;        
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
-        
+        boolean[][] dp = new boolean[2][s2.length() + 1];
         for (int i = 0; i <= s1.length(); i++) {
             for (int j = 0; j <= s2.length(); j++) {
+                int currRow = i % 2;
+                int prevRow= (i - 1) % 2;
                 int s3Idx = i + j - 1;
-              dp[i][j] = (i == 0 && j == 0) 
-                  ||(i > 0 && s1.charAt(i - 1) == s3.charAt(s3Idx) && dp[i - 1][j])
-                  || (j > 0 && s2.charAt(j - 1) == s3.charAt(s3Idx) && dp[i][j - 1]);
+              dp[currRow][j] = (i == 0 && j == 0) 
+                  ||(i > 0 && s1.charAt(i - 1) == s3.charAt(s3Idx) && dp[prevRow][j])
+                  || (j > 0 && s2.charAt(j - 1) == s3.charAt(s3Idx) && dp[currRow][j - 1]);
             }
         }
-        return dp[s1.length()][s2.length()];
+        return dp[s1.length() % 2][s2.length()];
     }
     
     public boolean isInterleave(String s1, String s2, String s3, int s1Idx, int s2Idx, int s3Idx, Boolean[][] dp) {
