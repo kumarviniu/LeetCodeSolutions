32c30c9b911c247517eb3399b7a9c320970c8fcd
kumarviniu <94697933+kumarviniu@users.noreply.github.com>
2022-09-27 00:32:24 -0400

    Time: 9 ms (96.50%), Space: 42.6 MB (96.46%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,15 +1,15 @@
 class Solution {
     public int minSteps(String s, String t) {
-        Map<Character, Integer> sFreq = new HashMap<>();
-        Map<Character, Integer> tFreq = new HashMap<>();
+        int[] sFreq = new int[26];
+        int[] tFreq = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            sFreq.put(s.charAt(i), sFreq.getOrDefault(s.charAt(i), 0) + 1);
-            tFreq.put(t.charAt(i), tFreq.getOrDefault(t.charAt(i), 0) + 1);
+            sFreq[s.charAt(i) - 'a']++;
+            tFreq[t.charAt(i) - 'a']++;
         }
         
         int minSteps = 0;
-        for (Character sKey : sFreq.keySet())
-            minSteps += sFreq.get(sKey) - Math.min(sFreq.get(sKey), tFreq.getOrDefault(sKey, 0));
+        for (int i = 0; i < 26; i++)
+            minSteps += sFreq[i] - Math.min(sFreq[i], tFreq[i]);
         
         return minSteps;
     }
